1aab692c7c274cfda6b387d3315dcbb0079608cc
ikeawesom <ikelimjl@gmail.com>
2025-04-22 17:38:39 +0800

    extracted helper functions

---
+++ src/lib/helpers.ts
@@ -0,0 +1,40 @@
+import { CodeTheme } from "./generator";
+
+export function getTableThemeClass(theme: CodeTheme): string {
+  switch (theme) {
+    case "light":
+      return "rounded-md overflow-hidden table-auto border border-gray-300";
+    case "dark":
+      return "rounded-md overflow-hidden table-auto bg-gray-800 text-white border border-gray-600";
+    case "material":
+      return "rounded-md overflow-hidden table-auto border border-gray-200 shadow-md";
+    case "minimal":
+      return "rounded-md overflow-hidden table-fixed border-collapse border border-slate-300";
+    default:
+      return "rounded-md overflow-hidden table-auto";
+  }
+}
+
+export function getBodyThemeClass(theme: CodeTheme): string {
+  switch (theme) {
+    case "light":
+      return "bg-gray-50 text-gray-950";
+    case "dark":
+      return "bg-gray-700 text-white";
+    case "material":
+      return "bg-blue-50/10 text-gray-950";
+    case "minimal":
+      return "";
+  }
+}
+
+export function toCamelCase(input: string): string {
+  return (
+    input
+      .replace(/[_\s-]+/g, " ")
+      .split(" ")
+      .map((word) => word.charAt(0).toUpperCase() + word.slice(1))
+      .join("")
+      .replace(/[^a-zA-Z0-9]/g, "") + "Table"
+  );
+}
